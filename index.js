65d330499ce405ee8ad76d37118642cb88e6b033
Anmoly6422 <231046@kit.ac.in>
2026-09-15 17:22:20 +0530

    url database completed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const {connectToMongoDB}= require('./connect');
+const urlRoute= require('./routes/url');
+
 
 const app = express();
 const PORT = 8001;
+
+
+connectToMongoDB('mongodb://localhost:27017/cutly')
+.then(()=>console.log('Mongodb connected'))
+
+app.use("/url",urlRoute);
+
+app.listen(PORT,()=> console.log(`Server Started at Port: ${PORT}`))
