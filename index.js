746c4d7925510634f2589b86d89a5e3eb6a3dec1
Anmoly6422 <231046@kit.ac.in>
2026-09-15 17:39:52 +0530

    Database connected

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const {connectToMongoDB}= require('./connect');
-const urlRoute= require('./routes/url');
+const dns = require("dns");
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+require("dotenv").config();
+
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
 
+connectToMongoDB(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB connection error:", err));
 
-connectToMongoDB('mongodb://localhost:27017/cutly')
-.then(()=>console.log('Mongodb connected'))
+app.use(express.json());
 
-app.use("/url",urlRoute);
+app.use("/url", urlRoute);
 
-app.listen(PORT,()=> console.log(`Server Started at Port: ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Server Started at Port: ${PORT}`);
+});
